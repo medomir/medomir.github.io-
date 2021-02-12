f92f5b60e96a32d3d4ce04cd022df8aea3a2bacc
medomir <bojansilni@gmail.com>
2021-02-12 16:52:01 +0100

    initial commit

---
+++ public/component---src-pages-404-js-c7bdab61cf0b897a29d8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),r=n.n(t),l=n("7oih"),o=n("Wbzz");n("EYWl");a.default=function(){return r.a.createElement(l.a,null,r.a.createElement("main",{className:"error-page"},r.a.createElement("div",{className:"error-container"},r.a.createElement("h2",null,"The page you were looking, does not exists!"),r.a.createElement("br",null),r.a.createElement(o.a,{to:"/",className:"btn"},"Back to Home"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c7bdab61cf0b897a29d8.js.map
